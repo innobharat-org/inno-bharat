e1cc6c02ce6c77233b9a51979e92168b8be744ce
Inno Bharat <innobharat.in@gmail.com>
2025-02-09 21:09:08 +0530

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -107,33 +107,15 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Collaborating with InnoBharat Team was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. InnoBharat Team's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, InnoBharat can be the best ideal partner.",
+    name: "Dovono",
+    title: "Founder of AlphaCrest Innovations",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Collaborating with InnoBharat Team was an absolute pleasure. Their professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. InnoBharat Team's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, InnoBharat can be the best ideal partner.",
+    name: "Vijay Vemula",
+    title: "Engineer at Amazon",
   },
 ];
 
